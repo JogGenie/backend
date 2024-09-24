aee8284de3c9d5698ed2ee52624691e262029091
Abdul Jaleel Khan <suf.khan2000@gmail.com>
2024-09-23 23:28:48 -0500

    Update controller.ts

--- src/database/controller.ts
+++ src/database/controller.ts
@@ -7,8 +7,22 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-pool.on('connect', () => {
-  console.log('Connected to the database');
-});
+export const connect = async () => {
+  try {
+    await pool.connect();
+    console.log('Connected to the database');
+  } catch (err) {
+    console.error('Error connecting to the database', err.message);
+  }
+};
+
+export const close = async () => {
+  try {
+    await pool.end();
+    console.log('Database connection closed');
+  } catch (err) {
+    console.error('Error closing the database connection', err.message);
+  }
+};
 
 export default pool;
